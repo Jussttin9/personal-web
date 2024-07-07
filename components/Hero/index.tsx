0c73de829e040eb5f8400b1c4430b83c67091e4a
jtran-9 <jr.tran79@gmail.com>
2024-07-07 02:14:41 -0700

    added animation for home page

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import styles from "./page.module.css";
 import Image from "next/image"
 import Nav from "../Nav";
@@ -7,21 +8,28 @@ export default function Hero() {
     return (
         <div>
             <Nav/>
-            <div className={styles.container}>
-                <div className={styles.title}>
-                    Justin&apos;s Portfolio
-                </div>
-                <div className={styles.image}>
-                    <Image 
-                    className={styles.img}
-                    src="/me.jpg"
-                    alt="a picture of me!"
-                    width={525}
-                    height={700}
-                    priority
-                    />
-                </div>
-            </div>
+            <motion.div
+                    initial={{ y: 10, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: -10, opacity: 0 }}
+                    transition={{ duration: 1 }}
+            >
+                <div className={styles.container}>
+                    <div className={styles.title}>
+                        Justin&apos;s Portfolio
+                    </div>
+                    <div className={styles.image}>
+                        <Image 
+                        className={styles.img}
+                        src="/me.jpg"
+                        alt="a picture of me!"
+                        width={525}
+                        height={700}
+                        priority
+                        />
+                    </div>
+                </div>  
+            </motion.div>
         </div>
     );
 }
